ea257685a15c1b61f1f53b053556770f10317a01
Alejandro Mery <amery@geeks.cl>
2012-04-24 20:25:37 +0200

    build: use INSTALL_LMOD from lua.pc to install the lua modules

--- src/lua/Makefile.am.sh
+++ src/lua/Makefile.am.sh
@@ -7,8 +7,10 @@ find_lua() {
 
 cd "${0%/*}"
 cat <<EOT | tee Makefile.am
+INSTALL_LMOD = @INSTALL_LMOD@
+
+luadatadir = \$(INSTALL_LMOD)
+
 nobase_dist_luadata_DATA = \\
 	$(find_lua sancus)
-
-luadatadir = @datadir@/lua/5.1
 EOT
