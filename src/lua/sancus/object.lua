a725a26aa89324006de05a1a91c3a99e513cd8db
Alejandro Mery <amery@geeks.cl>
2012-04-30 19:16:42 +0200

    lua: add sancus.object.Class

---
+++ src/lua/sancus/object.lua
@@ -0,0 +1,24 @@
+--
+
+local _class = {
+	__call = function (c, ...) return c:new(...) end,
+}
+
+function Class(c)
+	c = c or {}
+	c.__index = c
+
+	function c:new(o)
+		o = o or {}
+		setmetatable(o, c)
+		return o
+	end
+
+	setmetatable(c, _class)
+
+	return c
+end
+
+return {
+	Class = Class,
+}
