073ced81adf6dab59f5aa565e5637c6932557cea
Alejandro Mery <amery@geeks.cl>
2012-08-06 18:23:53 +0200

    model: add Decimal and Integer types

---
+++ src/lua/sancus/model/Decimal.lua
@@ -0,0 +1,42 @@
+-- This file is part of sancus-lua-core
+-- <https://github.com/sancus-project/sancus-lua-core>
+--
+-- Copyright (c) 2009 - 2012, Alejandro Mery <amery@geeks.cl>
+--
+
+local Class = require("sancus.object").Class
+local tonumber, type = tonumber, type
+
+setfenv(1, {})
+
+local Decimal = Class{
+	new = function(cls, model, name, default, min, max)
+		local f = model:prepare_add_field(name, cls.validate)
+
+		f.default = default
+		f.min = min
+		f.max = max
+
+		return f
+	end,
+}
+
+function Decimal.validate(f, v)
+	if type(v) == "string" then
+		v = tonumber(v)
+	end
+
+	if type(v) ~= 'number' then
+		-- NOP
+	elseif f.min and v < f.min then
+		-- NOP
+	elseif f.max and v > f.max then
+		-- NOP
+	else
+		-- valid number
+		return true, v
+	end
+	return false
+end
+
+return Decimal
---
+++ src/lua/sancus/model/Integer.lua
@@ -0,0 +1,37 @@
+-- This file is part of sancus-lua-core
+-- <https://github.com/sancus-project/sancus-lua-core>
+--
+-- Copyright (c) 2009 - 2012, Alejandro Mery <amery@geeks.cl>
+--
+
+local Class = require("sancus.object").Class
+local Decimal = assert(require"sancus.model.Decimal")
+local tonumber, type, floor = tonumber, type, math.floor
+
+setfenv(1, {})
+
+local function validate(f, v)
+	if type(v) == "string" then
+		v = tonumber(v)
+	end
+
+	if type(v) ~= 'number' or floor(v) ~= v then
+		return false
+	else
+		return Decimal.validate(f, v)
+	end
+end
+
+local Integer = Class{
+	new = function(cls, model, name, default, min, max)
+		local f = model:prepare_add_field(name, validate)
+
+		f.default = default
+		f.min = min
+		f.max = max
+
+		return f
+	end,
+}
+
+return Integer
