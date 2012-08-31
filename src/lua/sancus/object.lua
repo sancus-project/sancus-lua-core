a6c0fe02474caf3c1ebe1e72bef673ac02aebe02
Alejandro Mery <amery@geeks.cl>
2012-08-31 16:49:34 +0200

    object: Class: don't override :new() if provided

--- src/lua/sancus/object.lua
+++ src/lua/sancus/object.lua
@@ -8,10 +8,12 @@ function Class(c)
 	c = c or {}
 	c.__index = c
 
-	function c:new(o)
-		o = o or {}
-		setmetatable(o, c)
-		return o
+	if not c.new then
+		function c:new(o)
+			o = o or {}
+			setmetatable(o, self)
+			return o
+		end
 	end
 
 	setmetatable(c, _class)
