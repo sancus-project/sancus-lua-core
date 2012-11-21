449588ea960eb1873b3c0913723fdcc30717c6ec
Alejandro Mery <amery@geeks.cl>
2012-11-21 20:09:19 +0100

    build: add INSTALL_CMOD support

--- src/cmod/Makefile.am.sh
+++ src/cmod/Makefile.am.sh
@@ -14,9 +14,11 @@ cat <<EOT | tee Makefile.am
 AM_LDFLAGS = \$(SANCUS_LIBS) \$(LUA_LIBS) -module -avoid-version
 AM_CFLAGS = \$(SANCUS_CFLAGS) \$(LUA_CFLAGS)
 
+INSTALL_CMOD = @INSTALL_CMOD@
+
 nobase_cmod_LTLIBRARIES = sancus.la
 
 sancus_la_SOURCES = $(list *.c)
 
-cmoddir = @libdir@/lua/5.1
+cmoddir = \$(INSTALL_CMOD)
 EOT
