f4c4cb6d2c9577cf6748a938a0f81a09317ba713
Alejandro Mery <amery@geeks.cl>
2013-05-21 22:01:39 +0200

    object.Enum: introduce new object class
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

---
+++ src/lua/sancus/object/Enum.lua
@@ -0,0 +1,53 @@
+-- This file is part of sancus-lua-core
+-- <https://github.com/sancus-project/sancus-lua-core>
+--
+-- Copyright (c) 2013, Alejandro Mery <amery@geeks.cl>
+--
+
+local Class = require"sancus.object.Class"
+local utils = require"sancus.utils"
+
+local ipairs, rawget = ipairs, rawget
+
+setfenv(1, {})
+
+local function enum_iter(self, default)
+	local i = 1
+	return function(_, _)
+		local k = self._keys[i]
+		if k then
+			i = i + 1
+			return k, self._enum[k], k == default
+		end
+	end
+end
+
+Enum = {
+	init = function(cls, self)
+		self = self or {}
+
+		return {
+			_enum = self,
+			_keys = utils.sorted_keys(self),
+		}
+	end,
+
+	-- iterators
+	--
+	__ipairs = function(self)
+		return ipairs(self._keys)
+	end,
+	__pairs = enum_iter,
+	__call = enum_iter,
+
+	-- key access
+	--
+	__index = function(self, key)
+		return rawget(self._enum, key) or rawget(Enum, key)
+	end,
+	__newindex = function (self, key, value)
+		error("attempt to update a read-only table", 2)
+	end
+}
+
+return Class(Enum)
--- src/lua/sancus/object/init.lua
+++ src/lua/sancus/object/init.lua
@@ -1,7 +1,9 @@
 local Class = require"sancus.object.Class"
 local Table = require"sancus.object.Table"
+local Enum = require"sancus.object.Enum"
 
 return {
 	Class = Class,
 	Table = Table,
+	Enum = Enum,
 }
