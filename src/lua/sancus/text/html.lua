02180c98fba14a564e8038f9760c8ab9e81c9987
Alejandro Mery <amery@geeks.cl>
2013-05-29 13:01:12 +0200

    sancus.text.html: add dummy NOP HTML encoder
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

---
+++ src/lua/sancus/text/html.lua
@@ -0,0 +1,14 @@
+-- This file is part of sancus-lua-core
+-- <https://github.com/sancus-project/sancus-lua-core>
+--
+-- Copyright (c) 2013, Alejandro Mery <amery@geeks.cl>
+--
+
+local _M = { _NAME = ... }
+setfenv(1, _M)
+
+function encode(v)
+	return ""
+end
+
+return _M
